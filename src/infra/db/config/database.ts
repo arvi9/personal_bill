e6a2e389062b3fdb85f7ba1c67eaacaa5bca3c62
João Guilherme <joao.sczip@hotmail.com>
2020-07-29 13:12:23 -0300

    feat: add migrations config

--- src/infra/db/config/database.ts
+++ src/infra/db/config/database.ts
@@ -1,8 +1,13 @@
 import knex from "knex";
+import path from "path";
 
 const test = knex({
   client: "sqlite3",
   connection: ":memory:",
+  migrations: {
+    tableName: "migrations",
+    directory: path.resolve(__dirname, "..", "migrations"),
+  },
 });
 
 export default { test };
