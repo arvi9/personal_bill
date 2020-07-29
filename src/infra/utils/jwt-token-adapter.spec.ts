b8bf60b193eb6ddb2cc97802b96bb3502b8475d8
João Guilherme <joao.sczip@hotmail.com>
2020-07-28 23:06:55 -0300

    refactor: move sut creation to a factory method

--- src/infra/utils/jwt-token-adapter.spec.ts
+++ src/infra/utils/jwt-token-adapter.spec.ts
@@ -5,10 +5,13 @@ import { JwtTokenAdapter } from "./jwt-token-adapter";
 jest.mock("jsonwebtoken");
 const mockedJwt = jwt as jest.Mocked<typeof jwt>;
 
+const makeSut = (secretKey = faker.random.uuid()): JwtTokenAdapter =>
+  new JwtTokenAdapter(secretKey);
+
 describe("JwtTokenAdapter", () => {
   it("should calls jsonwebtoken with correct values", () => {
     const secretKey = faker.random.uuid();
-    const sut = new JwtTokenAdapter(secretKey);
+    const sut = makeSut(secretKey);
     const params = {
       email: faker.internet.email(),
       id: faker.random.uuid(),
