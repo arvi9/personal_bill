38665fec70d82443ea85a49de0b2d98dbd5243d8
João Guilherme <joao.sczip@hotmail.com>
2020-07-28 21:46:58 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/authenticate/db-authenticate.spec.ts
+++ src/data/usecases/authenticate/db-authenticate.spec.ts
@@ -2,10 +2,23 @@ import faker from "faker";
 import { AccountsRepositorySpy } from "@/data/tests";
 import { DbAuthenticate } from "./db-authenticate";
 
+type SutTypes = {
+  sut: DbAuthenticate;
+  accountsRepositorySpy: AccountsRepositorySpy;
+};
+
+const makeSut = (): SutTypes => {
+  const accountsRepositorySpy = new AccountsRepositorySpy();
+  const sut = new DbAuthenticate(accountsRepositorySpy);
+  return {
+    sut,
+    accountsRepositorySpy,
+  };
+};
+
 describe("DbAuthenticate", () => {
   it("should calls AccountsRepository.findByEmail with correct values", async () => {
-    const accountsRepositorySpy = new AccountsRepositorySpy();
-    const sut = new DbAuthenticate(accountsRepositorySpy);
+    const { sut, accountsRepositorySpy } = makeSut();
     const params = {
       email: faker.internet.email(),
       password: faker.internet.password(),
