7352646ef2b447789b429fe82d47e7a32fb38516
João Guilherme <joao.sczip@hotmail.com>
2020-07-29 18:08:24 -0300

    feat: add app config

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,5 @@
-import express from "express";
-import connection from "../infra/db/config/database";
-import Routes from "./routes/routes";
-
-const app = express();
-app.use(Routes);
+import app from "@/main/config/app";
+import connection from "@/infra/db/config/database";
 
 connection.create().then(() => {
   app.listen(8080, () => console.log("Listen on 8080"));
