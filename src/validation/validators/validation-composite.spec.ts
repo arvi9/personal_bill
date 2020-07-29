6c152faf004dbdaa327f806732baa5077c60f9ab
João Guilherme <joao.sczip@hotmail.com>
2020-07-29 12:14:18 -0300

    refactor: move sut creation to a factory method

--- src/validation/validators/validation-composite.spec.ts
+++ src/validation/validators/validation-composite.spec.ts
@@ -2,16 +2,28 @@ import faker from "faker";
 import { ValidationComposite } from "./validation-composite";
 import { ValidationSpy } from "@/presentation/test";
 
+type SutTypes = {
+  sut: ValidationComposite;
+  validationsSpy: ValidationSpy[];
+};
+
+const makeSut = (): SutTypes => {
+  const validationsSpy = [new ValidationSpy(), new ValidationSpy()];
+  const sut = new ValidationComposite(validationsSpy);
+  return {
+    sut,
+    validationsSpy,
+  };
+};
+
 describe("ValidationComposite", () => {
   it("should calls validate for each validation received", () => {
-    const validationSpy = new ValidationSpy();
-    const secondValidationSpy = new ValidationSpy();
-    const sut = new ValidationComposite([validationSpy, secondValidationSpy]);
+    const { sut, validationsSpy } = makeSut();
     const input = {
       field: faker.random.word(),
     };
     sut.validate(input);
-    expect(validationSpy.input).toEqual(input);
-    expect(secondValidationSpy.input).toEqual(input);
+    expect(validationsSpy[0].input).toEqual(input);
+    expect(validationsSpy[1].input).toEqual(input);
   });
 });
