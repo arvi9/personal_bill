b6e66fb3387e8cf878ca08695020dead8e371555
João Guilherme <joao.sczip@hotmail.com>
2020-07-28 23:30:01 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/authentication-controller.spec.ts
+++ src/presentation/controllers/authentication-controller.spec.ts
@@ -2,9 +2,18 @@ import faker from "faker";
 import { AuthenticationController } from "./authentication-controller";
 import { badRequest } from "../protocols/http";
 
+type SutTypes = {
+  sut: AuthenticationController;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new AuthenticationController();
+  return { sut };
+};
+
 describe("AuthenticationController", () => {
   it("should returns badRequest if email was not provided", async () => {
-    const sut = new AuthenticationController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: { email: faker.internet.email() },
     };
