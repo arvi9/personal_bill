68200973d7fb85502bb88b086973ecab476372a1
João Guilherme <joao.sczip@hotmail.com>
2020-07-29 11:57:20 -0300

    refactor: add validationSpy to its own file

---
+++ src/validation/test/mock-validation.ts
@@ -0,0 +1,9 @@
+import { Validation } from "@/presentation/protocols";
+
+export class ValidationSpy implements Validation {
+  input: any;
+  validate(input: any): Error {
+    this.input = input;
+    return null;
+  }
+}
--- src/validation/validators/validation-composite.spec.ts
+++ src/validation/validators/validation-composite.spec.ts
@@ -1,25 +1,17 @@
 import faker from "faker";
 import { ValidationComposite } from "./validation-composite";
-import { Validation } from "@/presentation/protocols";
-
-class ValidationMock implements Validation {
-  input: any;
-  validate(input: any): Error {
-    this.input = input;
-    return null;
-  }
-}
+import { ValidationSpy } from "@/presentation/test";
 
 describe("ValidationComposite", () => {
   it("should calls validate for each validation received", () => {
-    const validationMock = new ValidationMock();
-    const secondValidationMock = new ValidationMock();
-    const sut = new ValidationComposite([validationMock, secondValidationMock]);
+    const validationSpy = new ValidationSpy();
+    const secondValidationSpy = new ValidationSpy();
+    const sut = new ValidationComposite([validationSpy, secondValidationSpy]);
     const input = {
       field: faker.random.word(),
     };
     sut.validate(input);
-    expect(validationMock.input).toEqual(input);
-    expect(secondValidationMock.input).toEqual(input);
+    expect(validationSpy.input).toEqual(input);
+    expect(secondValidationSpy.input).toEqual(input);
   });
 });
