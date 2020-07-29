80773ecc73ef613ebf176cfa0c32317a646162f3
João Guilherme <joao.sczip@hotmail.com>
2020-07-28 22:43:17 -0300

    refactor: move sut creation to a factory method

--- src/infra/utils/bcrypt-adapter.spec.ts
+++ src/infra/utils/bcrypt-adapter.spec.ts
@@ -5,14 +5,16 @@ import { BcryptAdapter } from "./bcrypt-adapter";
 jest.mock("bcrypt");
 const mockedBcrypt = bcrypt as jest.Mocked<typeof bcrypt>;
 
+const makeSut = (): BcryptAdapter => new BcryptAdapter();
+
 describe("BcryptAdapter", () => {
-  it("should calls bcrypt with correct values", () => {
-    const sut = new BcryptAdapter();
+  it("should calls bcrypt with correct values", async () => {
+    const sut = makeSut();
     const params = {
       value: faker.random.word(),
       valueToCompare: faker.random.word(),
     };
-    sut.compare(params);
+    await sut.compare(params);
     expect(mockedBcrypt.compare).toHaveBeenCalledWith(
       params.value,
       params.valueToCompare
