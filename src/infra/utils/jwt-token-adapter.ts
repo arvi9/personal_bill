7d655b1bda73b481d1e76cde1d524c2bed66f826
João Guilherme <joao.sczip@hotmail.com>
2020-07-28 23:10:21 -0300

    feat: ensure JwtTokenAdapter return token on success

--- src/infra/utils/jwt-token-adapter.ts
+++ src/infra/utils/jwt-token-adapter.ts
@@ -5,7 +5,6 @@ export class JwtTokenAdapter implements GenerateAccessToken {
   constructor(private readonly secretKey: string) {}
 
   generate(params: GenerateAccessToken.Params): string {
-    jwt.sign(params, this.secretKey);
-    return null;
+    return jwt.sign(params, this.secretKey);
   }
 }
