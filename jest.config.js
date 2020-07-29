c143efcd2b369c45953bd685603aef588156de42
João Guilherme <joao.sczip@hotmail.com>
2020-07-29 17:45:04 -0300

    fix: add coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   roots: ["<rootDir>/src"],
   clearMocks: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/main/**",
+    "!<rootDir>/src/infra/db/migrations/**",
+  ],
   coverageDirectory: "coverage",
   transform: {
     ".+\\.ts$": "ts-jest",
