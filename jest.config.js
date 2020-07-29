7564ea69bce635b32a1de408c7db7ee85788e50f
João Guilherme <joao.sczip@hotmail.com>
2020-07-28 21:20:50 -0300

    chore: add and config jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  clearMocks: true,
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
+  coverageDirectory: "coverage",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      diagnostics: false,
+    },
+  },
+};
