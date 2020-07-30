0e6b82b48ae94036f9d6f35fb6753ba9f25dd248
João Guilherme <joao.sczip@hotmail.com>
2020-07-30 19:54:39 -0300

    test: ensure /login return 401 if email and password does not match

---
+++ src/infra/db/seeds/accounts-seed.ts
@@ -0,0 +1,12 @@
+import { Repository } from "typeorm";
+import { AccountModel } from "../models/account";
+import { mockAccount } from "@/domain/tests/mock-account";
+
+export const insertOneAccount = async (
+  repository: Repository<AccountModel>,
+  account = mockAccount()
+): Promise<void> => {
+      const created = repository.create(account);
+      await repository.save(created);
+    });
+};
--- src/main/routes/authentication.test.ts
+++ src/main/routes/authentication.test.ts
@@ -2,6 +2,10 @@ import faker from "faker";
 import request from "supertest";
 import app from "@/main/config/app";
 import connection from "@/infra/db/config/database";
+import { getRepository } from "typeorm";
+import { AccountModel } from "@/infra/db/models/account";
+import { mockAccount } from "@/domain/tests/mock-account";
+import { insertOneAccount } from "@/infra/db/seeds";
 
 describe("Authentication Route", () => {
   beforeAll(async () => {
@@ -14,6 +18,17 @@ describe("Authentication Route", () => {
     await connection.close();
   });
   it("should returns unauthorized if user was not found", (done) => {
+    request(app)
+      .post("/login")
+      .send({
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+      })
+      .expect(401, done);
+  });
+  it("should returns unauthorized if password does not match", async (done) => {
+    const repository = getRepository(AccountModel);
+    await insertOneAccount(repository);
     request(app)
       .post("/login")
       .send({
