077ee8c8e06f09b725cc5c6b477b18ef047282e0
João Guilherme <joao.sczip@hotmail.com>
2020-07-31 16:09:52 -0300

    test: ensure TypeOrmAddExpenseRepository add a new expense

---
+++ src/infra/db/repositories/expenses/typeorm-add-expense-repository.spec.ts
@@ -0,0 +1,28 @@
+import { TypeOrmAddExpenseRepository } from "./typeorm-add-expense-repository";
+import { mockAddExpense, mockAccount } from "@/domain/tests";
+import { getRepository } from "typeorm";
+import { ExpenseModel, AccountModel } from "@/infra/db/models";
+import connection from "@/infra/db/config/database";
+import { insertOneAccount } from "../../seeds";
+
+describe("TypeOrmAddExpenseRepository", () => {
+  beforeAll(async () => {
+    await connection.create();
+  });
+  beforeEach(async () => {
+    await connection.clear();
+  });
+  afterAll(async () => {
+    await connection.close();
+  });
+  it("should add a new expense to the database", async () => {
+    const repository = getRepository(ExpenseModel);
+    const accountRepository = getRepository(AccountModel);
+    const account = mockAccount();
+    await insertOneAccount(accountRepository, account);
+    const sut = new TypeOrmAddExpenseRepository();
+    await sut.add({ ...mockAddExpense(), account });
+    const expenses = await repository.find();
+    expect(expenses).toHaveLength(1);
+  });
+});
