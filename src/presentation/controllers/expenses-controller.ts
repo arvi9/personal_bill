3a6357c34fdf9528f0a952637ad2869862ed3e46
João Guilherme <joao.sczip@hotmail.com>
2020-07-30 21:56:43 -0300

    feat: ensure ExpensesController calls AddExpense with correct values

--- src/presentation/controllers/expenses-controller.ts
+++ src/presentation/controllers/expenses-controller.ts
@@ -4,15 +4,21 @@ import {
   badRequest,
 } from "@/presentation/protocols/http";
 import { Validation } from "@/presentation/protocols";
+import { AddExpense } from "@/domain/usecases";
 
 export class ExpensesController {
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly addExpense: AddExpense
+  ) {}
 
   async store(httpRequest: HttpRequest): Promise<HttpResponse> {
     const validationError = this.validation.validate(httpRequest.body);
     if (validationError) {
       return badRequest(validationError);
     }
+
+    await this.addExpense.add(httpRequest.body);
     return null;
   }
 }
