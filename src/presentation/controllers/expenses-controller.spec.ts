931568bfcfa62ada12a89bf1c6a949ff9f284582
João Guilherme <joao.sczip@hotmail.com>
2020-07-30 21:49:51 -0300

    feat: ensure ExpensesController returns bad request if validation fails

--- src/presentation/controllers/expenses-controller.spec.ts
+++ src/presentation/controllers/expenses-controller.spec.ts
@@ -1,6 +1,7 @@
 import { ExpensesController } from "./expenses-controller";
 import { mockAddExpense } from "@/domain/tests";
 import { ValidationSpy } from "@/presentation/test";
+import { badRequest } from "../protocols/http";
 
 type SutTypes = {
   sut: ExpensesController;
@@ -28,5 +29,11 @@ describe("ExpensesController", () => {
     const httpRequest = makeRequest();
     await sut.store(httpRequest);
     expect(validationSpy.input).toBe(httpRequest.body);
+  });
+  it("should returns bad request if validation fails", async () => {
+    const { sut, validationSpy } = makeSut();
+    validationSpy.validationError = new Error("Validation Error");
+    const httpResponse = await sut.store(makeRequest());
+    expect(httpResponse).toEqual(badRequest(new Error("Validation Error")));
   });
 });
--- src/presentation/controllers/expenses-controller.ts
+++ src/presentation/controllers/expenses-controller.ts
@@ -1,11 +1,18 @@
-import { HttpRequest, HttpResponse } from "@/presentation/protocols/http";
-import { Validation } from "../protocols";
+import {
+  HttpRequest,
+  HttpResponse,
+  badRequest,
+} from "@/presentation/protocols/http";
+import { Validation } from "@/presentation/protocols";
 
 export class ExpensesController {
   constructor(private readonly validation: Validation) {}
 
   async store(httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body);
+    const validationError = this.validation.validate(httpRequest.body);
+    if (validationError) {
+      return badRequest(validationError);
+    }
     return null;
   }
 }
