48f06f5fff63c04a0f75e0a9b09157049ec97ebd
João Guilherme <joao.sczip@hotmail.com>
2020-07-30 21:45:48 -0300

    refactor: move sut creation a factory method

--- src/presentation/controllers/expenses-controller.spec.ts
+++ src/presentation/controllers/expenses-controller.spec.ts
@@ -2,15 +2,30 @@ import { ExpensesController } from "./expenses-controller";
 import { mockAddExpense } from "@/domain/tests";
 import { ValidationSpy } from "@/presentation/test";
 
+type SutTypes = {
+  sut: ExpensesController;
+  validationSpy: ValidationSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy();
+  const sut = new ExpensesController(validationSpy);
+  return {
+    sut,
+    validationSpy,
+  };
+};
+
+const makeRequest = () => ({
+  body: {
+    ...mockAddExpense(),
+  },
+});
+
 describe("ExpensesController", () => {
   it("should calls validation with correct values", async () => {
-    const validationSpy = new ValidationSpy();
-    const sut = new ExpensesController(validationSpy);
-    const httpRequest = {
-      body: {
-        ...mockAddExpense(),
-      },
-    };
+    const { sut, validationSpy } = makeSut();
+    const httpRequest = makeRequest();
     await sut.store(httpRequest);
     expect(validationSpy.input).toBe(httpRequest.body);
   });
