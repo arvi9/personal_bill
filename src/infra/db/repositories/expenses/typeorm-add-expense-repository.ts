af19ccdd0d9c49c5dcce5c9c16dbe1689c4671b7
João Guilherme <joao.sczip@hotmail.com>
2020-07-31 16:37:25 -0300

    refactor: add repository to sut constructor

--- src/infra/db/repositories/expenses/typeorm-add-expense-repository.ts
+++ src/infra/db/repositories/expenses/typeorm-add-expense-repository.ts
@@ -1,19 +1,24 @@
-import { getRepository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 import { AddExpenseRepository } from "@/data/protocols";
 import { ExpenseModel } from "@/infra/db/models";
 
 export class TypeOrmAddExpenseRepository implements AddExpenseRepository {
+  private repository: Repository<ExpenseModel>;
+
+  constructor() {
+    this.repository = getRepository(ExpenseModel);
+  }
+
   async add(
     params: AddExpenseRepository.Params
   ): Promise<AddExpenseRepository.Model> {
-    const repository = getRepository(ExpenseModel);
-    const expense = repository.create({
+    const expense = this.repository.create({
       description: params.description,
       date: params.date,
       accountId: params.account.id,
       value: params.value,
     });
 
-    return repository.save(expense);
+    return this.repository.save(expense);
   }
 }
