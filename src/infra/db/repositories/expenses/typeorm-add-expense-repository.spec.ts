02c843280245f482b9e6a73bf2392170396872d1
João Guilherme <joao.sczip@hotmail.com>
2020-07-31 16:31:07 -0300

    refactor: move sut creation a factory method

--- src/infra/db/repositories/expenses/typeorm-add-expense-repository.spec.ts
+++ src/infra/db/repositories/expenses/typeorm-add-expense-repository.spec.ts
@@ -5,6 +5,9 @@ import { ExpenseModel, AccountModel } from "@/infra/db/models";
 import connection from "@/infra/db/config/database";
 import { insertOneAccount } from "../../seeds";
 
+const makeSut = (): TypeOrmAddExpenseRepository =>
+  new TypeOrmAddExpenseRepository();
+
 describe("TypeOrmAddExpenseRepository", () => {
   beforeAll(async () => {
     await connection.create();
@@ -15,12 +18,12 @@ describe("TypeOrmAddExpenseRepository", () => {
   afterAll(async () => {
     await connection.close();
   });
-  it("should add a new expense to the database", async () => {
+  it.only("should add a new expense to the database", async () => {
     const repository = getRepository(ExpenseModel);
     const accountRepository = getRepository(AccountModel);
     const account = mockAccount();
     await insertOneAccount(accountRepository, account);
-    const sut = new TypeOrmAddExpenseRepository();
+    const sut = makeSut();
     await sut.add({ ...mockAddExpense(), account });
     const expenses = await repository.find();
     expect(expenses).toHaveLength(1);
