eda549abb5e1d44051ff6af7df2721c1029be3d2
João Guilherme <joao.sczip@hotmail.com>
2020-08-03 18:45:09 -0300

    feat: add expenses router to routes

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { authenticationRouter } from "./authentication";
+import { expensesRouter } from "./expenses";
 
 const router = Router();
 router.use(authenticationRouter);
+router.use(expensesRouter);
 
 export default router;
