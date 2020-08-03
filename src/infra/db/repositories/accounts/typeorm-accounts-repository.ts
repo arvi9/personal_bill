5702f3fe661004ccd884f3c991770e282922a68f
João Guilherme <joao.sczip@hotmail.com>
2020-08-03 18:01:47 -0300

    feat: ensure TypeOrmAccountsRepository returns an account on loadByToken success

--- src/infra/db/repositories/accounts/typeorm-accounts-repository.ts
+++ src/infra/db/repositories/accounts/typeorm-accounts-repository.ts
@@ -2,12 +2,17 @@ import { EntityRepository, getRepository, Repository } from "typeorm";
 import {
   LoadAccountByEmailRepository,
   UpdateAccessTokenRepository,
+  LoadAccountByTokenRepository,
 } from "@/data/protocols";
 import { AccountModel } from "@/infra/db/models/account";
+import { LoadAccountByToken } from "@/domain/usecases";
 
 @EntityRepository(AccountModel)
 export class TypeOrmAccountsRepository
-  implements LoadAccountByEmailRepository, UpdateAccessTokenRepository {
+  implements
+    LoadAccountByEmailRepository,
+    UpdateAccessTokenRepository,
+    LoadAccountByTokenRepository {
   private repository: Repository<AccountModel>;
 
   constructor() {
@@ -35,5 +40,19 @@ export class TypeOrmAccountsRepository
     const account = await this.repository.findOne(params.accountId);
     account.accessToken = params.accessToken;
     await this.repository.save(account);
+  }
+
+  async loadByToken(accessToken: string): Promise<LoadAccountByToken.Model> {
+    const {
+      created_at,
+      updated_at,
+      ...account
+    } = await this.repository.findOne({
+      where: {
+        accessToken,
+      },
+    });
+
+    return account;
   }
 }
