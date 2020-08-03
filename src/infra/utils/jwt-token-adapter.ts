05e0deaebfbd7edecf248bae78db1d1bbed8aa35
João Guilherme <joao.sczip@hotmail.com>
2020-08-03 17:39:00 -0300

    feat: ensure JwtTokenAdapter calls verify with correct values

--- src/infra/utils/jwt-token-adapter.ts
+++ src/infra/utils/jwt-token-adapter.ts
@@ -1,10 +1,15 @@
 import jwt from "jsonwebtoken";
-import { GenerateAccessToken } from "@/data/protocols";
+import { GenerateAccessToken, Decrypter } from "@/data/protocols";
 
-export class JwtTokenAdapter implements GenerateAccessToken {
+export class JwtTokenAdapter implements GenerateAccessToken, Decrypter {
   constructor(private readonly secretKey: string) {}
 
   generate(params: GenerateAccessToken.Params): string {
     return jwt.sign(params, this.secretKey);
+  }
+
+  async decrypt(value: string): Promise<string> {
+    jwt.verify(value, this.secretKey);
+    return null;
   }
 }
