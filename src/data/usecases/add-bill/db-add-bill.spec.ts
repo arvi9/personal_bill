028e1cf786436ddae5ef048d009976d0981f9f53
João Guilherme <joao.sczip@hotmail.com>
2020-08-04 20:51:22 -0300

    refactor: move sut creation to factory method

--- src/data/usecases/add-bill/db-add-bill.spec.ts
+++ src/data/usecases/add-bill/db-add-bill.spec.ts
@@ -2,10 +2,23 @@ import { DbAddBill } from "./db-add-bill";
 import { mockBill } from "@/domain/tests";
 import { AddBillRepositorySpy } from "@/data/tests";
 
+type SutTypes = {
+  sut: DbAddBill;
+  addBillRepositorySpy: AddBillRepositorySpy;
+};
+
+const makeSut = (): SutTypes => {
+  const addBillRepositorySpy = new AddBillRepositorySpy();
+  const sut = new DbAddBill(addBillRepositorySpy);
+  return {
+    sut,
+    addBillRepositorySpy,
+  };
+};
+
 describe("DbAddBill", () => {
   it("should calls AddBillRepository with correct values", async () => {
-    const addBillRepositorySpy = new AddBillRepositorySpy();
-    const sut = new DbAddBill(addBillRepositorySpy);
+    const { sut, addBillRepositorySpy } = makeSut();
     const bill = mockBill();
     await sut.add(bill);
     expect(addBillRepositorySpy.params).toEqual(bill);
