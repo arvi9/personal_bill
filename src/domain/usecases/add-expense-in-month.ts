c7074c021831164a6cbc97fd987bb6fdc21116b5
João Guilherme <joao.sczip@hotmail.com>
2020-08-05 20:59:15 -0300

    feat: update AddExpenseInMonth

--- src/domain/usecases/add-expense-in-month.ts
+++ src/domain/usecases/add-expense-in-month.ts
@@ -1,11 +1,10 @@
-import { Expense } from "@/domain/models";
-
 export interface AddExpenseInMonth {
   add: (params: AddExpenseInMonth.Params) => Promise<void>;
 }
 
 export namespace AddExpenseInMonth {
   export type Params = {
-    expense: Expense;
+    value: number;
+    date: Date;
   };
 }
