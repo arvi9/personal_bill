09d7aac7856572c6cf8234b250829602d37b664d
João Guilherme <joao.sczip@hotmail.com>
2020-08-04 21:05:16 -0300

    feat: ensure AddBillController returns 400 if validation fails

--- src/presentation/controllers/add-bill-controller.ts
+++ src/presentation/controllers/add-bill-controller.ts
@@ -3,13 +3,17 @@ import {
   HttpRequest,
   HttpResponse,
   Validation,
+  badRequest,
 } from "@/presentation/protocols";
 
 export class AddBillController implements Controller {
   constructor(private readonly validation: Validation) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body);
+    const validationError = this.validation.validate(httpRequest.body);
+    if (validationError) {
+      return badRequest(validationError);
+    }
     return null;
   }
 }
