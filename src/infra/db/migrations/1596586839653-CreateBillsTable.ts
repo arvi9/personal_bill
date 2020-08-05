129b75eca6ceffc8e7afa3fa93a331f555c95ed0
João Guilherme <joao.sczip@hotmail.com>
2020-08-04 21:22:16 -0300

    feat: add bills migration

---
+++ src/infra/db/migrations/1596586839653-CreateBillsTable.ts
@@ -0,0 +1,67 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateBillsTable1596586839653 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "bills",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            length: "36",
+            generationStrategy: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "account_id",
+            type: "varchar",
+            length: "36",
+          },
+          {
+            name: "description",
+            type: "varchar",
+            length: "128",
+          },
+          {
+            name: "value",
+            type: "decimal",
+            length: "10, 2",
+          },
+          {
+            name: "due_date",
+            type: "integer",
+          },
+          {
+            name: "expiration_date",
+            type: "date",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "bills_account_id_foreign",
+            columnNames: ["account_id"],
+            referencedColumnNames: ["id"],
+            referencedTableName: "accounts",
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("bills");
+  }
+}
