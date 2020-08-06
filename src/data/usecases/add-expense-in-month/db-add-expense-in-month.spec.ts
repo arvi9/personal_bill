d9f542d4cf49a6f731af60ebdd2064de23b9bacc
João Guilherme <joao.sczip@hotmail.com>
2020-08-05 22:00:05 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/add-expense-in-month/db-add-expense-in-month.spec.ts
+++ src/data/usecases/add-expense-in-month/db-add-expense-in-month.spec.ts
@@ -3,11 +3,24 @@ import { DbAddExpenseInMonth } from "./db-add-expense-in-month";
 import { mockAccount } from "@/domain/tests";
 import { LoadMonthlyExpenseByDateRepositorySpy } from "@/data/tests";
 
+type SutTypes = {
+  sut: DbAddExpenseInMonth;
+  loadMonthlyExpenseByDateRepositorySpy: LoadMonthlyExpenseByDateRepositorySpy;
+};
+
+const makeSut = (): SutTypes => {
+  const loadMonthlyExpenseByDateRepositorySpy = new LoadMonthlyExpenseByDateRepositorySpy();
+  const sut = new DbAddExpenseInMonth(loadMonthlyExpenseByDateRepositorySpy);
+  return {
+    sut,
+    loadMonthlyExpenseByDateRepositorySpy,
+  };
+};
+
 describe("DbAddExpenseInMonth", () => {
   it("should calls LoadMonthlyExpensesByDateRepository with correct values", async () => {
     const account = mockAccount();
-    const loadMonthlyExpenseByDateRepositorySpy = new LoadMonthlyExpenseByDateRepositorySpy();
-    const sut = new DbAddExpenseInMonth(loadMonthlyExpenseByDateRepositorySpy);
+    const { sut, loadMonthlyExpenseByDateRepositorySpy } = makeSut();
     const params = {
       account: {
         id: account.id,
