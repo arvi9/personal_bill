325b599d412bd198ca7c666c1b0963da4d5799e1
João Guilherme <joao.sczip@hotmail.com>
2020-08-06 22:20:58 -0300

    test: ensure DbAddExpense calls notifyListeners with correct data

--- src/data/tests/db/mock-add-expenses-repository.ts
+++ src/data/tests/db/mock-add-expenses-repository.ts
@@ -1,11 +1,18 @@
+import faker from "faker";
 import { AddExpenseRepository } from "@/data/protocols";
+import { mockAddExpense, mockAccount } from "@/domain/tests";
 
 export class AddExpenseRepositoryMock implements AddExpenseRepository {
   params: any;
+  expense = {
+    ...mockAddExpense(),
+    account: mockAccount(),
+    id: faker.random.uuid(),
+  };
   async add(
     params: AddExpenseRepository.Params
   ): Promise<AddExpenseRepository.Model> {
     this.params = params;
-    return null;
+    return Promise.resolve(this.expense);
   }
 }
--- src/data/usecases/add-expense/db-add-expense.spec.ts
+++ src/data/usecases/add-expense/db-add-expense.spec.ts
@@ -1,20 +1,26 @@
 import faker from "faker";
 import { DbAddExpense } from "./db-add-expense";
-import { AddExpenseRepositoryMock } from "@/data/tests";
+import { AddExpenseRepositoryMock, ExpenseListenerMock } from "@/data/tests";
 import { mockAddExpense, mockAccount } from "@/domain/tests/";
 import { AddExpense } from "@/domain/usecases";
 
 type SutTypes = {
   sut: DbAddExpense;
   addExpenseRepositoryMock: AddExpenseRepositoryMock;
+  expenseListenerMocks: ExpenseListenerMock[];
 };
 
 const makeSut = (): SutTypes => {
   const addExpenseRepositoryMock = new AddExpenseRepositoryMock();
-  const sut = new DbAddExpense(addExpenseRepositoryMock);
+  const expenseListenerMocks = [
+    new ExpenseListenerMock(),
+    new ExpenseListenerMock(),
+  ];
+  const sut = new DbAddExpense(addExpenseRepositoryMock, expenseListenerMocks);
   return {
     sut,
     addExpenseRepositoryMock,
+    expenseListenerMocks,
   };
 };
 
@@ -38,5 +44,20 @@ describe("DbAddExpense", () => {
       .mockResolvedValueOnce(createdExpense);
     const result = await sut.add(expense);
     expect(result).toEqual(createdExpense);
+  });
+  it("should calls notifyListeners with correct data", async () => {
+    const { sut, addExpenseRepositoryMock, expenseListenerMocks } = makeSut();
+    const data = {
+      account: {
+        id: addExpenseRepositoryMock.expense.account.id,
+      },
+      date: addExpenseRepositoryMock.expense.date,
+      value: addExpenseRepositoryMock.expense.value,
+      amount: 1,
+    };
+
+    await sut.add(mockAddExpense());
+    expect(expenseListenerMocks[0].params).toEqual(data);
+    expect(expenseListenerMocks[1].params).toEqual(data);
   });
 });
