8e2f4210a7476488a3f2ed0b0efe14ebc5e76f95
João Guilherme <joao.sczip@hotmail.com>
2020-08-06 23:34:29 -0300

    test: ensure TypeOrmMonthlyExpensesRepository.findByDate returns expenses

--- src/infra/db/repositories/monthly-expenses/typeorm-monthly-expenses-repository.spec.ts
+++ src/infra/db/repositories/monthly-expenses/typeorm-monthly-expenses-repository.spec.ts
@@ -2,8 +2,8 @@ import { getConnection, getRepository } from "typeorm";
 import connection from "@/infra/db/config/database";
 import { TypeOrmMonthlyExpensesRepository } from "./typeorm-monthly-expenses-repository";
 import { insertOneAccount } from "../../seeds";
-import { AccountModel } from "../../models";
-import { mockAccount } from "@/domain/tests";
+import { AccountModel, MonthlyExpensesModel } from "../../models";
+import { mockAccount, mockMonthlyExpense } from "@/domain/tests";
 
 describe("TypeOrmMonthlyExpensesRepository", () => {
   beforeAll(async () => {
@@ -22,12 +22,34 @@ describe("TypeOrmMonthlyExpensesRepository", () => {
       const sut = new TypeOrmMonthlyExpensesRepository();
       const account = mockAccount();
       await insertOneAccount(getRepository(AccountModel), account);
-      await sut.loadByDate({
+
+      const monthlyExpenseRepository = getRepository(MonthlyExpensesModel);
+      const monthlyOne = mockMonthlyExpense();
+      const monthlyTwo = mockMonthlyExpense();
+      monthlyOne.account = account;
+      monthlyOne.month = 8;
+      monthlyOne.year = 2020;
+      delete monthlyOne.id;
+      monthlyTwo.account = account;
+      monthlyTwo.month = 9;
+      monthlyTwo.year = 2020;
+      delete monthlyTwo.id;
+      const createdModels = monthlyExpenseRepository.create([
+        monthlyOne,
+        monthlyTwo,
+      ]);
+      await monthlyExpenseRepository.save(createdModels);
+
+      const result = await sut.loadByDate({
         account: {
           id: account.id,
         },
         date: new Date(2020, 7, 20),
       });
+      expect(result).toHaveLength(1);
+      expect(result[0].month).toBe(8);
+      expect(result[0].year).toBe(2020);
+      expect(result[0].value).toBe(monthlyOne.value);
     });
   });
 });
