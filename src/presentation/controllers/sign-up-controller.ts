ac426b3a7a282394cc670b0423131d402c7d3deb
João Guilherme <joao.sczip@hotmail.com>
2020-08-10 22:40:51 -0300

    feat: ensure SignUpController calls SignUp with correct values

--- src/presentation/controllers/sign-up-controller.ts
+++ src/presentation/controllers/sign-up-controller.ts
@@ -5,15 +5,26 @@ import {
   Validation,
   badRequest,
 } from "@/presentation/protocols";
+import { SignUp } from "@/domain/usecases";
 
 export class SignUpController implements Controller {
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly signUp: SignUp
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const validationError = this.validation.validate(httpRequest.body);
     if (validationError) {
       return badRequest(validationError);
     }
+
+    const { email, name, password } = httpRequest.body;
+    await this.signUp.signup({
+      email,
+      name,
+      password,
+    });
     return null;
   }
 }
