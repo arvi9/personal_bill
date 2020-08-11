64949e60d904b20a5b844422c91290bbaee3428b
João Guilherme <joao.sczip@hotmail.com>
2020-08-10 21:40:31 -0300

    feat: ensure DbSignUp calls GenerateAccessToken with correct values

--- src/data/usecases/sign-up/db-sign-up.ts
+++ src/data/usecases/sign-up/db-sign-up.ts
@@ -3,22 +3,28 @@ import {
   LoadAccountByEmailRepository,
   Hasher,
   AddAccountRepository,
+  GenerateAccessToken,
 } from "@/data/protocols";
 
 export class DbSignUp implements SignUp {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly generateAccessToken: GenerateAccessToken
   ) {}
 
   async signup(params: SignUp.Params): Promise<SignUp.Model> {
     await this.loadAccountByEmailRepository.loadByEmail(params.email);
     const hashedPassword = await this.hasher.hash(params.password);
-    await this.addAccountRepository.add({
+    const account = await this.addAccountRepository.add({
       email: params.email,
       name: params.name,
       password: hashedPassword,
+    });
+    this.generateAccessToken.generate({
+      id: account.id,
+      email: account.email,
     });
     return null;
   }
