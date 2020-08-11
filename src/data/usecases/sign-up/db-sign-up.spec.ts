58ad8c544c4204eec1fa97f7b6e98050886b1f2b
João Guilherme <joao.sczip@hotmail.com>
2020-08-10 21:14:51 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/sign-up/db-sign-up.spec.ts
+++ src/data/usecases/sign-up/db-sign-up.spec.ts
@@ -2,16 +2,31 @@ import faker from "faker";
 import { DbSignUp } from "./db-sign-up";
 import { AccountsRepositorySpy } from "@/data/tests";
 
+type SutTypes = {
+  sut: DbSignUp;
+  loadAccountByEmailSpy: AccountsRepositorySpy;
+};
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailSpy = new AccountsRepositorySpy();
+  const sut = new DbSignUp(loadAccountByEmailSpy);
+  return {
+    sut,
+    loadAccountByEmailSpy,
+  };
+};
+
+const makeFakeParams = () => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+  passwordConfirmation: this.password,
+});
+
 describe("DbSignUp", () => {
   it("should calls LoadAccountByEmail repository with correct email", async () => {
-    const loadAccountByEmailSpy = new AccountsRepositorySpy();
-    const sut = new DbSignUp(loadAccountByEmailSpy);
-    const params = {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-      passwordConfirmation: this.password,
-    };
+    const { sut, loadAccountByEmailSpy } = makeSut();
+    const params = makeFakeParams();
     await sut.signup(params);
     expect(loadAccountByEmailSpy.email).toBe(params.email);
   });
