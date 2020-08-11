050d20f2ff2d6527fbbdf122879a77411340addf
João Guilherme <joao.sczip@hotmail.com>
2020-08-11 20:00:42 -0300

    test: ensure POST /signup returns 400 if email address is already in use

---
+++ src/main/routes/sign-up/sign-up.test.ts
@@ -0,0 +1,36 @@
+import request from "supertest";
+import faker from "faker";
+import { getConnection, getRepository } from "typeorm";
+import app from "@/main/config/app";
+import connection from "@/infra/db/config/database";
+import { mockAccount } from "@/domain/tests";
+import { insertOneAccount } from "@/infra/db/seeds";
+import { AccountModel } from "@/infra/db/models";
+
+describe("SignUp Route", () => {
+  beforeAll(async () => {
+    await connection.create();
+    await getConnection().runMigrations();
+  });
+  beforeEach(async () => {
+    await connection.clear();
+  });
+  afterAll(async () => {
+    await getConnection().undoLastMigration();
+    await connection.close();
+  });
+  it("should returns 400 if email address is already in use", async () => {
+    const dbAccount = {
+      ...mockAccount(),
+      email: faker.internet.email(),
+    };
+    await insertOneAccount(getRepository(AccountModel), dbAccount);
+    const { accessToken, id, ...fakeAccount } = mockAccount();
+    const accountRequest = {
+      ...fakeAccount,
+      email: dbAccount.email,
+      passwordConfirmation: fakeAccount.password,
+    };
+    await request(app).post("/signup").send(accountRequest).expect(400);
+  });
+});
