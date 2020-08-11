2b1265d299dbf8aa59749003bfc1ef8f4d987868
João Guilherme <joao.sczip@hotmail.com>
2020-08-10 22:33:19 -0300

    refactor: move sut creation to afactory method

--- src/presentation/controllers/sign-up-controller.spec.ts
+++ src/presentation/controllers/sign-up-controller.spec.ts
@@ -1,20 +1,38 @@
 import faker from "faker";
 import { SignUpController } from "./sign-up-controller";
 import { ValidationSpy } from "@/presentation/test";
+import { HttpRequest } from "../protocols";
+
+type SutTypes = {
+  sut: SignUpController;
+  validationSpy: ValidationSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy();
+  const sut = new SignUpController(validationSpy);
+  return {
+    sut,
+    validationSpy,
+  };
+};
+
+const makeFakeRequest = (): HttpRequest => {
+  const password = faker.internet.password();
+  return {
+    body: {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password,
+      passwordConfirmation: password,
+    },
+  };
+};
 
 describe("SignUpController", () => {
   it("should calls validation with correct params", async () => {
-    const validationSpy = new ValidationSpy();
-    const sut = new SignUpController(validationSpy);
-    const password = faker.internet.password();
-    const params = {
-      body: {
-        name: faker.name.findName(),
-        email: faker.internet.email(),
-        password,
-        passwordConfirmation: password,
-      },
-    };
+    const { sut, validationSpy } = makeSut();
+    const params = makeFakeRequest();
     await sut.handle(params);
     expect(validationSpy.input).toEqual(params.body);
   });
